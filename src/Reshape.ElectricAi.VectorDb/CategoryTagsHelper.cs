2d8ed66fec32d6419140d9917daa077e161cad5b
Cristian Mazilu <mazilu6@gmail.com>
2026-05-24 07:05:36 +0300

    Make everything case-insensitive.

--- src/Reshape.ElectricAi.VectorDb/CategoryTagsHelper.cs
+++ src/Reshape.ElectricAi.VectorDb/CategoryTagsHelper.cs
@@ -6,7 +6,7 @@ internal static class CategoryTagsHelper
 {
     internal static string[] ToTags(IReadOnlyDictionary<Category, IReadOnlyList<string>> categoryValues) =>
         categoryValues
-            .SelectMany(kvp => kvp.Value.Select(v => $"{kvp.Key}.{v}"))
+            .SelectMany(kvp => kvp.Value.Select(v => $"{kvp.Key}.{v}".ToLowerInvariant()))
             .ToArray();
 
 }
